bafa0faf9be644f080f11814d573c45395c8cb7e
gabrielglpacheco-code <gabrielglpacheco@gmail.com>
2026-06-03 18:22:53 -0300

    feat: add interfaces de pokemon

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 console.log("pikachu, um pokemon eletrico...")
+
+import type { PokemonResumo } from "./models/Pokemon"
+
+const pikachu: PokemonResumo = {
+    id: 25,
+    nome: "pikachu",
+    tipos: ["electric"],
+    altura: 4,
+    peso: 60,
+    hp: 35,
+    ataque: 55,
+    defesa: 40
+};
+
+console.log(pikachu);
